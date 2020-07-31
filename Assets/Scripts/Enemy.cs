639ef3ab2bef4fd58b51f10407f62f6171594252
blcoskey <blcoskey@gmail.com>
2020-07-31 18:27:14 +0200

    Implementing monster chase

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent Mob;
+    public GameObject Player;
+    public float MobDistanceRun = 4.0f;
+
+    [SerializeField]
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mob = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+        if(distance < MobDistanceRun){
+            animator.SetBool("Chase", true);
+            Vector3 directionToPlayer = transform.position - Player.transform.position;
+            Vector3 newPos = transform.position - directionToPlayer;
+
+            Mob.SetDestination(newPos);
+        }
+    }
+}
