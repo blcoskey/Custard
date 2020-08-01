2d74dd1e0ec9df8e97e0eed636d372e3e2c5e53a
blcoskey <blcoskey@gmail.com>
2020-08-01 22:09:27 +0200

    AI done

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Linq;
+
+public class HuntTarget {
+    public Transform transform { get; set; }
+    public float distance { get; set; }
+}
 
 public class Enemy : MonoBehaviour {
     [SerializeField]
@@ -11,7 +17,7 @@ public class Enemy : MonoBehaviour {
     [Header ("Relations")]
     [SerializeField]
     private MazeManager mazeManager;
-    public GameObject Player;
+    public GameObject player;
     public float mobDistanceRun = 4.0f;
 
     [SerializeField]
@@ -22,10 +28,19 @@ public class Enemy : MonoBehaviour {
     private float walkSpeed = 5.0f;
     [SerializeField]
     private float runSpeed = 10.0f;
+
+    [Header ("Navigation")]
     [SerializeField]
-    private float lineOfSight = 20.0f;
-    
-    [Header("Navigation")]
+    private float lineOfSightDistance = 5.0f;
+    [SerializeField]
+    private Vector3 personalLastSighting;
+    [SerializeField]
+    private SphereCollider senseCollider;
+    [SerializeField]
+    private float fieldOfViewAngle = 110.0f;
+
+    [SerializeField]
+    private bool playerInSight = false;
 
     [SerializeField]
     private int lastMaze = 0;
@@ -42,65 +57,137 @@ public class Enemy : MonoBehaviour {
         mob.speed = walkSpeed;
         animator.SetBool ("Patrol", true);
         lastMaze = mazeManager.currentMaze;
-        currentPatrolPath = GetPatrolPath();
+        currentPatrolPath = GetPatrolPath ();
+        senseCollider = GetComponent<SphereCollider> ();
 
         GotoNextPoint ();
     }
 
-    // Update is called once per frame
     void Update () {
         switch (currentState) {
-            case EnemyState.Patrol:
+            case EnemyState.Hunt:
                 PatrolUpdate ();
+                break;
+            case EnemyState.Patrol: 
+                PatrolUpdate ();
+                break;
+            case EnemyState.Chase:
+                Chase ();
                 break;
 
         }
-        // float distance = Vector3.Distance (transform.position, Player.transform.position);
+    }
 
-        // if (distance < mobDistanceRun) {
-        //     animator.SetBool ("Chase", true);
-        //     Vector3 directionToPlayer = transform.position - Player.transform.position;
-        //     Vector3 newPos = transform.position - directionToPlayer;
+    void OnTriggerStay (Collider other) {
+        if (other.gameObject == player) {
+            playerInSight = false;
+            Vector3 playerDirection = other.transform.position - transform.position;
+            float angle = Vector3.Angle (playerDirection, transform.forward);
 
-        //     mob.SetDestination (newPos);
-        // }
+            if (angle < fieldOfViewAngle * 0.5f) {
+                if (Physics.Raycast (transform.position + transform.up, playerDirection.normalized, out RaycastHit raycastHit, lineOfSightDistance)) {
+                    Debug.DrawRay (transform.position + transform.up, playerDirection.normalized * raycastHit.distance, Color.yellow);
+                    if (raycastHit.collider.gameObject.tag == "Player") {
+                        playerInSight = true;
+                        personalLastSighting = other.transform.position;
+                        ChangeState (currentState, EnemyState.Chase);
+                    }
+                }
+            }
+        }
+    }
+
+    private void Chase () {
+        float distance = Vector3.Distance (transform.position, player.transform.position);
+
+        if (distance < mobDistanceRun) {
+            animator.SetBool ("Chase", true);
+            Vector3 directionToPlayer = transform.position - player.transform.position;
+            Vector3 newPos = transform.position - directionToPlayer;
+
+            mob.SetDestination (newPos);
+        } else {
+            ChangeState(currentState, EnemyState.Hunt);
+        }
+    }
+
+    private void Hunt () {
+
+    }
+
+    private List<Transform> GetHuntPath(){
+        List<HuntTarget> huntLocations = new List<HuntTarget> ();
+
+        foreach (Transform location in currentPatrolPath) {
+            float distance = (location.position - personalLastSighting).sqrMagnitude;
+            huntLocations.Add (new HuntTarget { transform = location, distance = distance });
+        }
+
+        return huntLocations.OrderBy(x => x.distance).Select(x => x.transform).ToList();
+    }
+
+    private void ChangeState (EnemyState prevState, EnemyState newState) {
+
+        switch (prevState) {
+            case EnemyState.Patrol:
+                animator.SetBool ("Patrol", false);
+                break;
+            case EnemyState.Chase:
+                animator.SetBool ("Chase", false);
+                break;
+
+        }
+
+        switch (newState) {
+            case EnemyState.Patrol:
+                mob.speed = walkSpeed;
+                animator.SetBool ("Patrol", true);
+                break;
+            case EnemyState.Chase:
+                mob.speed = runSpeed;
+                animator.SetBool ("Chase", true);
+                break;
+            case EnemyState.Hunt:
+                mob.speed = runSpeed;
+                animator.SetBool ("Chase", true);
+                currentPatrolPath = GetHuntPath();
+                break;
+        }
+
+        currentState = newState;
     }
 
     private void PatrolUpdate () {
-        if(lastMaze != mazeManager.currentMaze){
+        if (lastMaze != mazeManager.currentMaze) {
             lastMaze = mazeManager.currentMaze;
-            currentPatrolPath = GetPatrolPath();
+            currentPatrolPath = GetPatrolPath ();
         }
         if (!mob.pathPending && mob.remainingDistance < 0.5f)
             GotoNextPoint ();
     }
 
-    void GotoNextPoint () {
-        // Returns if no points have been set up
+    private void GotoNextPoint () {
         if (currentPatrolPath.Count == 0)
             return;
 
-        // Set the agent to go to the currently selected destination.
         mob.SetDestination (currentPatrolPath[destPoint].position);
 
-        // Choose the next point in the array as the destination,
-        // cycling to the start if necessary.
         destPoint = (destPoint + 1) % currentPatrolPath.Count;
     }
 
-    private List<Transform> GetPatrolPath(){
-        var newPath = new List<Transform>();
-        newPath.AddRange(patrolPoints);
+    private List<Transform> GetPatrolPath () {
+        var newPath = new List<Transform> ();
+        newPath.AddRange (patrolPoints);
 
-        switch(mazeManager.currentMaze){
+        switch (mazeManager.currentMaze) {
             case 1:
-                newPath.AddRange(maze1PatrolPoints);
+                newPath.AddRange (maze1PatrolPoints);
                 break;
             case 2:
-                newPath.AddRange(maze2PatrolPoints);
+                newPath.AddRange (maze2PatrolPoints);
                 break;
             case 3:
-                newPath.AddRange(maze3PatrolPoints);
+                newPath.AddRange (maze3PatrolPoints);
                 break;
         }
 
