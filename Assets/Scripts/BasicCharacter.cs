89340e46c839290fbd50d885a294c4238ec8a351
blcoskey <blcoskey@gmail.com>
2020-08-02 02:31:29 +0200

    Added in the rest of Custard's animations

--- Assets/Scripts/BasicCharacter.cs
+++ Assets/Scripts/BasicCharacter.cs
@@ -2,6 +2,8 @@
 using UnityEngine;
 
 public class BasicCharacter : MonoBehaviour {
+  [Header ("AnimationVariables")]
+  [SerializeField]
   private static readonly int WALK_PROPERTY = Animator.StringToHash ("Walk");
 
   [Header ("Stats")]
@@ -42,16 +44,16 @@ public class BasicCharacter : MonoBehaviour {
 
   [Header ("Relations")]
   [SerializeField]
-  private Animator animator = null;
+  private Animator animator;
 
   [SerializeField]
-  private Rigidbody physicsBody = null;
+  private Rigidbody physicsBody;
 
   [SerializeField]
-  private SpriteRenderer spriteRenderer = null;
+  private SpriteRenderer spriteRenderer;
 
   [SerializeField]
-  private Light candleLight = null;
+  private Light candleLight;
   [SerializeField]
   private UIBarManager fearBar;
   [SerializeField]
@@ -98,17 +100,16 @@ public class BasicCharacter : MonoBehaviour {
     float inputX = 0;
     if (Input.GetKey (KeyCode.RightArrow)) {
       inputX = 1;
-      spriteRenderer.flipX = false;
+      // spriteRenderer.flipX = false;
     } else if (Input.GetKey (KeyCode.LeftArrow)) {
       inputX = -1;
-      spriteRenderer.flipX = true;
+      // spriteRenderer.flipX = true;
     }
 
     // Normalize
     _movement = new Vector3 (inputX, 0, inputY).normalized;
 
-    animator.SetBool (WALK_PROPERTY,
-      Math.Abs (_movement.sqrMagnitude) > Mathf.Epsilon);
+    SetAnimation ();
   }
 
   private void FixedUpdate () {
@@ -184,7 +185,16 @@ public class BasicCharacter : MonoBehaviour {
       return true;
 
     return false;
+  }
 
+  private void SetAnimation () {
+    animator.SetBool (WALK_PROPERTY,
+      Math.Abs (_movement.sqrMagnitude) > Mathf.Epsilon);
+
+    animator.SetBool ("HasRedKey", keyType == KeyType.Red);
+    animator.SetBool ("HasGreenKey", keyType == KeyType.Green);
+    animator.SetBool ("HasBlueKey", keyType == KeyType.Blue);
+    animator.SetBool ("CandleOn", candleOn);
   }
 
 }
